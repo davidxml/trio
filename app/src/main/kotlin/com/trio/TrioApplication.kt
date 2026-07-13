a53ad562b26af8a143c19fe32b3cfa54cdde2ef6
David Adewunmi <pycodegenius@gmail.com>
2026-07-13 03:52:45 +0100

    feat: add Hilt dependency injection and application entry point
    - TrioApplication with @HiltAndroidApp annotation
    - AppModule, RepositoryModule, ServiceModule (empty @Module stubs)
    - DI layer configured for SingletonComponent

---
+++ app/src/main/kotlin/com/trio/TrioApplication.kt
@@ -0,0 +1,7 @@
+package com.trio
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TrioApplication : Application()
---
+++ app/src/main/kotlin/com/trio/core/di/RepositoryModule.kt
@@ -0,0 +1,9 @@
+package com.trio.core.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RepositoryModule
