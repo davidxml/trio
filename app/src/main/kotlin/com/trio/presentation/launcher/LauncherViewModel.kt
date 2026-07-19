a8b02ea0929cf8422d953966719c57bcc881efc0
David Adewunmi <pycodegenius@gmail.com>
2026-07-19 01:13:45 +0100

    feat: wire DI and refactor ViewModel to use DataStore via use cases
    - Convert RepositoryModule to abstract class with @Binds for ModeRepository
    - Refactor LauncherViewModel to use ObserveCurrentModeUseCase + SwitchModeUseCase
    - Remove GlobalModeStateHolder dependency in favor of DataStore flow

--- app/src/main/kotlin/com/trio/presentation/launcher/LauncherViewModel.kt
+++ app/src/main/kotlin/com/trio/presentation/launcher/LauncherViewModel.kt
@@ -1,20 +1,33 @@
 package com.trio.presentation.launcher
 
 import androidx.lifecycle.ViewModel
-import com.trio.data.state.GlobalModeStateHolder
+import androidx.lifecycle.viewModelScope
 import com.trio.domain.model.DeviceMode
+import com.trio.domain.usecase.ObserveCurrentModeUseCase
+import com.trio.domain.usecase.SwitchModeUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class LauncherViewModel @Inject constructor(
-    private val stateHolder: GlobalModeStateHolder
+    observeCurrentModeUseCase: ObserveCurrentModeUseCase,
+    private val switchModeUseCase: SwitchModeUseCase
 ) : ViewModel() {
 
-    val mode: StateFlow<DeviceMode> = stateHolder.mode
+    val mode: StateFlow<DeviceMode> = observeCurrentModeUseCase()
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5_000),
+            initialValue = DeviceMode.STANDARD
+        )
 
     fun setMode(m: DeviceMode) {
-        stateHolder.setMode(m)
+        viewModelScope.launch {
+            switchModeUseCase(m)
+        }
     }
 }
